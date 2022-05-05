6bd8e200ee2ed542c4d23a32a1df581d18d5a483
delesslin <delesslingw@gmail.com>
2022-05-05 14:30:20 -0400

    :rocket: background image layers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import layer_0 from './assets/layers/layer_0.png'
+import layer_1 from './assets/layers/layer_1.png'
+import layer_2 from './assets/layers/layer_2.png'
+import layer_3 from './assets/layers/layer_3.png'
+import layer_4 from './assets/layers/layer_4.png'
 
+const ImageLayer = ({ url }) => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${url})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        position: 'absolute',
+        height: '100vh',
+        width: '100vw',
+      }}
+    />
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className='App'
+      style={{
+        height: '100vh',
+        width: '100vw',
+        position: 'relative',
+      }}
+    >
+      <ImageLayer url={layer_0} />
+      <ImageLayer url={layer_1} />
+      <ImageLayer url={layer_2} />
+      <ImageLayer url={layer_3} />
+      <ImageLayer url={layer_4} />
+      <div style={{ position: 'absolute', height: '100vh', width: '100vw' }}>
+        <p style={{ backgroundColor: 'red' }}>Test</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
